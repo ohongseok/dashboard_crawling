17848ffff86336773fea78fb04b6e805a9410d9b
ohongseok <picarovisual@gmail.com>
2026-08-03 18:59:38 +0900

    Support Korean app password

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def require_password():
             st.error("앱 비밀번호가 설정되지 않았습니다.")
             st.stop()
 
-        if hmac.compare_digest(password, expected_password):
+        if hmac.compare_digest(\n            password.encode("utf-8"), expected_password.encode("utf-8")\n        ):
             st.session_state["app_authenticated"] = True
             st.rerun()
         else:
